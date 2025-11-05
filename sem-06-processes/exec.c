ce1a8a7e541a812af1a94af6daf7ff187a1170c4
Dmitry Gulevich <gulevich.ds@phystech.edu>
2025-11-05 08:54:03 +0300

    add sem-06-processes

---
+++ sem-06-processes/exec.c
@@ -0,0 +1,17 @@
+#include <err.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char** argv) {
+    printf("starting program\n");
+
+    char* cmd = "mkdir";
+    argv[0] = cmd;
+    int res = execvp("mkdir", argv);
+    // code below executing only if error occurred
+    if (res < 0) {
+        err(1, "execvp");
+    }
+    printf("exiting\n");
+}
